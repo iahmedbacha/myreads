e9ae526b353ff2caec0f2b37856d20fb71b6bf78
Ibrahim AHMED BACHA <fi_ahmed_bacha@esi.dz>
2020-01-24 19:29:00 +0100

    fix: add key for children in BooksGrid

--- src/components/BooksGrid.js
+++ src/components/BooksGrid.js
@@ -3,7 +3,13 @@ import Book from './Book';
 
 const BooksGrid = (props) => (
   <ol className="books-grid">
-    { props.books.map(book => <Book book={book} />) }
+    { props.books.map(book =>
+        <Book
+          key={book.industryIdentifiers.filter(industryIdentifier => industryIdentifier.type === 'ISBN_13')[0].identifier}
+          book={book}
+        />
+      )
+    }
   </ol>
 );
 
