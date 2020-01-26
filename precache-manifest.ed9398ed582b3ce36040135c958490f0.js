6cf4f90cafb443e52a0d06a40bb046f22462a004
Ibrahim AHMED BACHA <fi_ahmed_bacha@esi.dz>
2020-01-26 21:55:04 +0100

    Updates

---
+++ precache-manifest.ed9398ed582b3ce36040135c958490f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d63f4b1cd37806e9d240543e32343f",
+    "url": "/myreads-app/index.html"
+  },
+  {
+    "revision": "77fbaad2a43157a96c44",
+    "url": "/myreads-app/static/css/main.bcafd81e.chunk.css"
+  },
+  {
+    "revision": "d515e627ef6afe8a46ed",
+    "url": "/myreads-app/static/js/2.e62ef23f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/myreads-app/static/js/2.e62ef23f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77fbaad2a43157a96c44",
+    "url": "/myreads-app/static/js/main.1282e6b8.chunk.js"
+  },
+  {
+    "revision": "4be6ea9a1b73e4949ecc",
+    "url": "/myreads-app/static/js/runtime-main.48a704a7.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads-app/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myreads-app/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads-app/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
